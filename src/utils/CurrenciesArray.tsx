133548c9642615951ef28902a91ef5961d5d25a6
Mariana Balanchuk <113304141+asteriab@users.noreply.github.com>
2023-03-28 12:36:05 +0200

    removed extra comments

--- src/utils/CurrenciesArray.tsx
+++ src/utils/CurrenciesArray.tsx
@@ -22,54 +22,5 @@ const CurrenciesArray: Currency[] = [
     currency: CurrencyConstants.UAH,
     rate: 1,
   },
-  //   {
-  //     currencyA: CurrencyConstants.USD,
-  //     currencyB: CurrencyConstants.EUR,
-  //     rate: 0.93,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.USD,
-  //     currencyB: CurrencyConstants.GBP,
-  //     rate: 0.81,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.USD,
-  //     currencyB: CurrencyConstants.UAH,
-  //     rate: 36.92,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.EUR,
-  //     currencyB: CurrencyConstants.GBP,
-  //     rate: 0.88,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.EUR,
-  //     currencyB: CurrencyConstants.UAH,
-  //     rate: 39.89,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.EUR,
-  //     currencyB: CurrencyConstants.USD,
-  //     rate: 1.08,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.GBP,
-  //     currencyB: CurrencyConstants.UAH,
-  //     rate: 45.4,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.GBP,
-  //     currencyB: CurrencyConstants.EUR,
-  //     rate: 1.14,
-  //   },
-  //   {
-  //     currencyA: CurrencyConstants.GBP,
-  //     currencyB: CurrencyConstants.USD,
-  //     rate: 1.23,
-  //   },
 ];
-
-// export const getProductsObject = (array: Product[]) =>
-//   array.reduce((object, product) => ({ ...object, [product.id]: product }), {});
-
 export default CurrenciesArray;
